0cb591fdd5b2d0ae97389bf3d9b11b76ce67bbae
Taofeek Adeniyi <105851811+tee4tao@users.noreply.github.com>
2023-08-12 20:42:21 +0100

    worked on the homepage footer

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,14 @@
-import { FaBars, FaTimes } from "react-icons/fa";
+// import { FaBars, FaTimes } from "react-icons/fa";
 import { Modal } from "./Modal";
 import { useGlobalContext } from "./context";
+import {
+  FaBars,
+  FaTimes,
+  FaTwitter,
+  FaWhatsapp,
+  FaGithub,
+  FaLinkedinIn,
+} from "react-icons/fa";
 export let Home = () => {
   let { openToggle, openModal } = useGlobalContext();
   return (
@@ -16,6 +24,32 @@ export let Home = () => {
         </button>
       </h3>
       <Modal />
+      <div className="home-footer">
+        <div className="footer-info">
+          <h3>
+            &copy;
+            <span id="date"> {new Date().getFullYear()} </span>
+            Calculator Built by TEE4TAO
+          </h3>
+        </div>
+        <div className="social-contacts">
+          <a href="https://linkedin.com/in/tee4tao" className="social-link">
+            <FaLinkedinIn />
+          </a>
+          <a href="https://github.com/tee4tao" className="social-link">
+            <FaGithub />
+          </a>
+          <a href="https://twitter.com/tee4tao" className="social-link">
+            <FaTwitter />
+          </a>
+          <a
+            href="https://api.whatsapp.com/send/?phone=%2B2348165013325&text&type=phone_number&app_absent=0"
+            className="social-link"
+          >
+            <FaWhatsapp />
+          </a>
+        </div>
+      </div>
     </section>
   );
 };
